07fc810f0abf984cf068d250a3da9dfeeaa5be6d
Sky Davis <skysdavis@gmail.com>
2017-11-15 21:24:48 -0500

    Adding packages neccesary

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 let gulp = require('gulp');
+const pck = require('./package.json');
+
+const minifyCSS = require('gulp-clean-css');
 
 gulp.task('default', function() {
 // Here add code
+});
+
+gulp.task('build', function() {
+  // here return the group of .scss files
+});
+
+gulp.task('minify', ['build'], function() {
+  // here minify and create friendly-atoms to .min
+  return gulp.src(["./dist/friendly-atoms.css"])
+    .pipe(minifyCSS())
+    .pipe(concat("friendly-atoms.css"))
+    .pipe(gulp.dest('./dist/'));
 });
